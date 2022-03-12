cc69b6e13c7e9db8ef72adc7231bc489728cfb91
Muhammad Harris <harris20014@gmail.com>
2022-03-12 18:07:37 +0500

    Update TicTacToe.h

--- src/TicTacToe.h
+++ src/TicTacToe.h
@@ -4,12 +4,12 @@ class TicTacToe {
 private:
 	char Board[3][3]; // O for Player 1, X for Player 2
 	bool GameOver; // tells whether game is over or not
-public:
 	void ResetBoard(); // print all values in board according to index
 	void PrintBoard(); // print board
 	bool BoardIsFull(); // returns true while board is full
 	void InputPlayerOne(); // get input from player 1
 	void InputPlayerTwo(); // get input from player 2
 	void Result(); // displays result
+public:
 	void Run(); // executes game
 };
