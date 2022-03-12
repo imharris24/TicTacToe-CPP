6f7a0e3f0d274e4b82479b764088d20cb3b3c5ec
Muhammad Harris <harris20014@gmail.com>
2022-03-12 18:05:07 +0500

    Update TicTacToe.cpp

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -14,16 +14,10 @@ void TicTacToe::ResetBoard() {
 	}
 	GameOver = false;
 }
-void TicTacToe::PrintValues() {
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << Board[i][j] << "   ";
-		}
-		cout << "\n";
-	}
-}
 void TicTacToe::PrintBoard() {
+	cout << "\n";
 	for (int i = 0; i < 3; i++) {
+		cout << "\t";
 		for (int j = 0; j < 3; j++) {
 			cout << " " << Board[i][j] << " ";
 			if (j == 0 || j == 1) {
@@ -31,25 +25,28 @@ void TicTacToe::PrintBoard() {
 			}
 		}
 		if (i == 0 || i == 1) {
-			cout << "\n-----------\n";
+			cout << "\n\t-----------\n";
 		}
 		else {
 			cout << "\n";
 		}
 	}
+	cout << "\n";
 }
 bool TicTacToe::BoardIsFull() {
 	if ((Board[0][0] == 'X' || Board[0][0] == 'O') && (Board[0][1] == 'X' || Board[0][1] == 'O') && (Board[0][1] == 'X' || Board[0][2] == 'O') && (Board[1][0] == 'X' || Board[1][0] == 'O') && (Board[1][1] == 'X' || Board[1][1] == 'O') && (Board[1][2] == 'X' || Board[1][2] == 'O') && (Board[2][0] == 'X' || Board[2][0] == 'O') && (Board[2][1] == 'X' || Board[2][1] == 'O') && (Board[2][2] == 'X' || Board[2][2] == 'O')) {
+		GameOver = true;
 		return true;
 	}
 	else return false;
 }
 void TicTacToe::InputPlayerOne() {
+	system("color 0A");
 	char Index;
 	while (true) {
 		if (!GameOver) {
 			Index = '\0';
-			cout << "Input Player 1: ";
+			cout << "\n\tInput Player 1: ";
 			Index = _getche();
 			cout << "\n";
 			for (int i = 0; i < 3; i++) {
@@ -60,7 +57,7 @@ void TicTacToe::InputPlayerOne() {
 					}
 				}
 			}
-			cout << "\nInvalid Input\n";
+			cout << "\n\tInvalid Input\n";
 		}
 		else {
 			return;
@@ -68,11 +65,12 @@ void TicTacToe::InputPlayerOne() {
 	}
 }
 void TicTacToe::InputPlayerTwo() {
+	system("color 04");
 	char Index;
 	while (true) {
 		if (!GameOver) {
 			Index = '\0';
-			cout << "Input Player 2: ";
+			cout << "\n\tInput Player 2: ";
 			Index = _getche();
 			cout << "\n";
 			for (int i = 0; i < 3; i++) {
@@ -83,7 +81,7 @@ void TicTacToe::InputPlayerTwo() {
 					}
 				}
 			}
-			cout << "\nInvalid Input\n";
+			cout << "\n\tInvalid Input\n";
 		}
 		else {
 			return;
@@ -94,20 +92,37 @@ void TicTacToe::Result() {
 	if ((Board[0][0] == 'O' && Board[0][1] == 'O' && Board[0][2] == 'O') || (Board[1][0] == 'O' && Board[1][1] == 'O' && Board[1][2] == 'O') || (Board[2][0] == 'O' && Board[2][1] == 'O' && Board[2][2] == 'O') || (Board[0][0] == 'O' && Board[1][0] == 'O' && Board[2][0] == 'O') || (Board[0][1] == 'O' && Board[1][1] == 'O' && Board[2][1] == 'O') || (Board[0][2] == 'O' && Board[1][2] == 'O' && Board[2][2] == 'O') || (Board[0][0] == 'O' && Board[1][1] == 'O' && Board[2][2] == 'O') || (Board[0][2] == 'O' && Board[1][1] == 'O' && Board[2][0] == 'O')) {
 		system("cls");
 		PrintBoard();
-		cout << "\nPlayer 1 WINS\n";
+		cout << "\n\tPlayer 1 WINS!\n";
 		GameOver = true;
+		cout << "\n\t";
+		system("pause");
+		cout << "\n";
 	}
 	else if ((Board[0][0] == 'X' && Board[0][1] == 'X' && Board[0][2] == 'X') || (Board[1][0] == 'X' && Board[1][1] == 'X' && Board[1][2] == 'X') || (Board[2][0] == 'X' && Board[2][1] == 'X' && Board[2][2] == 'X') || (Board[0][0] == 'X' && Board[1][0] == 'X' && Board[2][0] == 'X') || (Board[0][1] == 'X' && Board[1][1] == 'X' && Board[2][1] == 'X') || (Board[0][2] == 'X' && Board[1][2] == 'X' && Board[2][2] == 'X') || (Board[0][0] == 'X' && Board[1][1] == 'X' && Board[2][2] == 'X') || (Board[0][2] == 'X' && Board[1][1] == 'X' && Board[2][0] == 'X')) {
 		system("cls");
 		PrintBoard();
-		cout << "\nPlayer 2 WINS\n";
+		cout << "\n\tPlayer 2 WINS!\n";
 		GameOver = true;
+		cout << "\n\t";
+		system("pause");
+		cout << "\n";
 	}
+	else if (!((Board[0][0] == 'O' && Board[0][1] == 'O' && Board[0][2] == 'O') || (Board[1][0] == 'O' && Board[1][1] == 'O' && Board[1][2] == 'O') || (Board[2][0] == 'O' && Board[2][1] == 'O' && Board[2][2] == 'O') || (Board[0][0] == 'O' && Board[1][0] == 'O' && Board[2][0] == 'O') || (Board[0][1] == 'O' && Board[1][1] == 'O' && Board[2][1] == 'O') || (Board[0][2] == 'O' && Board[1][2] == 'O' && Board[2][2] == 'O') || (Board[0][0] == 'O' && Board[1][1] == 'O' && Board[2][2] == 'O') || (Board[0][2] == 'O' && Board[1][1] == 'O' && Board[2][0] == 'O')) && !((Board[0][0] == 'X' && Board[0][1] == 'X' && Board[0][2] == 'X') || (Board[1][0] == 'X' && Board[1][1] == 'X' && Board[1][2] == 'X') || (Board[2][0] == 'X' && Board[2][1] == 'X' && Board[2][2] == 'X') || (Board[0][0] == 'X' && Board[1][0] == 'X' && Board[2][0] == 'X') || (Board[0][1] == 'X' && Board[1][1] == 'X' && Board[2][1] == 'X') || (Board[0][2] == 'X' && Board[1][2] == 'X' && Board[2][2] == 'X') || (Board[0][0] == 'X' && Board[1][1] == 'X' && Board[2][2] == 'X') || (Board[0][2] == 'X' && Board[1][1] == 'X' && Board[2][0] == 'X')) && (GameOver == true)) {
+		system("cls");
+		PrintBoard();
+		cout << "\n\Its a DRAW!\n";
+		GameOver = true;
+		cout << "\n\t";
+		system("pause");
+		cout << "\n";
+	}
+
 }
 void TicTacToe::Run() {
 	char Option = '\0';
 	bool Swapper = true;
 	while (true) {
+		system("color 0F");
 		system("cls");
 		cout << "\n\t TIC-TAC-TOE\n";
 		cout << "\t1. Start Game\n";
@@ -131,18 +146,19 @@ void TicTacToe::Run() {
 						InputPlayerTwo();
 						Swapper = true;
 					}
-					if (GameOver == true && BoardIsFull() == true) {
-						system("cls");
-						PrintBoard();
-						cout << "\nIts a DRAW\n";
-						system("pause");
-						break;
-					}
 				}
 				else {
 					system("pause");
 					break;
 				}
+			}
+			if (GameOver == true && BoardIsFull() == true) {
+				system("cls");
+				PrintBoard();
+				cout << "\n\tIts a DRAW!\n";
+				cout << "\n\t";
+				system("pause");
+				break;
 			}
 			break;
 		case '2':
